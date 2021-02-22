233fe40369a18f96b3c17e51be0566c9dd251e1f
“Aaron <github email address>
2021-02-22 18:50:42 -0500

    greet(time) functioning

--- index.js
+++ index.js
@@ -9,5 +9,28 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let time1 = time.split(`:`);
+  let time2 = time1[0].concat(time1[1])
+  let aggregate = parseInt(time2, 10)
+
+
+  if (aggregate < 1200) {
+    return `Good Morning`;
+  }
+  else if (aggregate > 1700) {
+    return `Good Evening`;
+  }
+  else {
+    return `Good Afternoon`
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage() {
+  
+}
